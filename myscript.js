f7ed85a79ae3e3605709d62aa24b23cd9cb2eff8
Ximena Castillo <xcastillo537@gmail.com>
2019-07-17 10:30:52 -0500

    First Commit

---
+++ myscript.js
@@ -0,0 +1,59 @@
+function showtime() {
+  document.getElementById('demo').innerHTML = Date();
+};
+//   return num * 3;
+// }
+// // curly brackets for functions and ifelse statements
+// function timesFive(num) {return num * 5};
+//
+// function helloWorld() { alert("Hello World")};
+//
+// var name;
+// name = "Ximena";
+// // can be overidden
+//
+// let lastName = "Castillo"
+// // can be overidden
+//
+// const initials = "XC"
+// // no overidding
+//
+// if (5 == 5) {
+//   alert("Yay!")
+// };
+//
+// var age = 16;
+// age = age + 1;
+//
+// var distance = .2;
+// distance = "Arrived!";
+//
+// // let text = ""
+// // for (i = 0; i < 5; i++) {
+// //   text += "Num: " + i;
+// // }
+// //
+// // let text = ""
+// // var i = 0;
+// // while (i < 10) {
+// //   text += "Num: " + i;
+// //   i += 2;
+// // }
+//
+// var hour = 15
+// if (hour < 18) {
+//   greeting = "Good day";
+// }
+//
+// function hello() {
+//   return "hello";
+// }
+
+// to print stuff to the console
+// console.log("Hello World");
+// let num = 0
+//
+// // debugger checks variables at certain times, helps when you rename them
+// debugger;
+// num = 1;
+// console.log(num);
